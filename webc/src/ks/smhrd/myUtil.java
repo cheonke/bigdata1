f41411717b465b547494606656a8be5d64b5b110
smhrd <smhrd@DESKTOP-57CLACN>
2020-09-07 16:44:53 +0900

    - 수정 했다

--- webc/src/ks/smhrd/myUtil.java
+++ webc/src/ks/smhrd/myUtil.java
@@ -4,9 +4,9 @@ public class myUtil {
 	// 1 ~10까지의 총 합을 구하는 동작 
 	
 	public int hap() {
-		// 길동아 여기 좀 수정해봐 
+	//수정완료 
 		int sum=0;
-		for (int i=1;i<=10;i++) {
+		for (int i=1;i<=100;i++) {
 			sum+=i;
 		}
 	return sum;
